88fcb2e293cb14e0d125e42bdcc6fc33edf50e67
Vincent Hilla <vincent.hilla@rwth-aachen.de>
2022-04-15 15:20:49 +0200

    revise profiler

--- staticprofiler.py
+++ staticprofiler.py
@@ -1,9 +1,11 @@
 from threading import Lock
 import time
+from functools import wraps
 
 class Profiler:
     __data = {}
     __lock = Lock()
+    __print_always = True
 
     @classmethod
     def profile(cls, func):
@@ -21,6 +23,8 @@ class Profiler:
                 cls.__data[func.__name__]['time'] += (end - start)
             finally:
                 cls.__lock.release()
+            if cls.__print_always:
+                print(f'{func.__name__} took {end - start} seconds')
         
         return wrapper
 
